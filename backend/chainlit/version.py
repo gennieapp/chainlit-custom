28746196271927786a0797dc9c8c9f41ec02ef33
Rafael Silva <rjs@cesar.org.br>
2024-09-29 16:16:02 -0300

    Use correct package name and version when generating config.toml file

--- backend/chainlit/version.py
+++ backend/chainlit/version.py
@@ -1,7 +1,7 @@
 from importlib import metadata
 
 try:
-    __version__ = metadata.version(__package__)
+    __version__ = metadata.version('chainlit-custom')
 except metadata.PackageNotFoundError:
     # Case where package metadata is not available.
     __version__ = ""
